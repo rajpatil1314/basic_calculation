fbd3231b5964d9c1a1bb687ab4013ca441090740
rajpatil1314 <168514582+rajpatil1314@users.noreply.github.com>
2024-06-28 06:21:09 +0000

    average

---
+++ twentyone.js
@@ -0,0 +1,7 @@
+var a = prompt("enter 01 no");
+var b = prompt("enter 02 no");
+
+var sum = parseInt(a)+parseInt(b);
+var avg = sum / 2;
+var check = (a>0 && b>0) ? alert(avg) : alert("negative no");
+
